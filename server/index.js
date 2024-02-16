c4b10d4d84906c513618ce597aadec47d9b62273
A Badr <abdelrhman.badr@vodafone.com>
2024-02-16 22:40:55 +0000

    initial

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const port = 443;
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'test2' })
+});
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`)
+});
