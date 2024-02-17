f7d0bb608aa6f584356c813a67f7820c8dbcbdda
A Badr <abdelrhman.badr@vodafone.com>
2024-02-17 12:53:07 +0000

    logging

--- server/index.js
+++ server/index.js
@@ -5,12 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    console.log('body.params', req.params);
-    console.log('body.body', req.body);
+app.use((req, res, next) => {
+    console.log('method', req.method);
+    console.log('url', req.url);
+    console.log('params', req.params);
+    console.log('query', req.query);
+    console.log('body', req.body);
+    console.log('================');
+    next();
+});
 
+app.get('/', (req, res) => {
     res.status(200).send({ message: 'test2' })
 });
+
 
 app.listen(port, () => {
     console.log(`app is running on port ${port}`)
