820f167026a448bd08d846ccf3749a48cbb2a629
A Badr <abdelrhman.badr@vodafone.com>
2024-02-17 12:40:24 +0000

    logging

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    console.log('body.params', req.params);
+    console.log('body.body', req.body);
+
     res.status(200).send({ message: 'test2' })
 });
 
